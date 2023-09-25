217e0f1991c390c84ea1c317ab4c54b6db8623e0
Samuel Muli <samuelmuli95@gmail.com>
2023-09-25 16:59:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    const yearWon = record.find(item => item.result === 'W')
+
+    if(yearWon){
+        return yearWon.year
+    }else{
+        return undefined
+    }
+}
